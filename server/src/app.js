eee819d2418d4af18f7acacbeeade5b0f03bc0ed
NurTheDev <nur756.islam@gmail.com>
2025-07-22 21:59:38 +0600

    working

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const router = require("./routes/index");
 require("dotenv").config();
-
+const cors = require("cors");
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", router)
